6eefbc35261277a2f1b8da6fb8957b9115e46c56
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2021-09-11 09:36:52 +0500

    SDSD

---
+++ pages/home/__id.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div style="display:flex;">
+            <img v-for="image in home.images" :key="image" :src="image" alt="image" width="200" height="150"/>
+        </div>
+        <div>{{home.title}}</div>
+        <img src="/images/marker.svg" alt="marker.svg" width="20" height="20"> {{home.location.address}} {{home.location.city}} {{home.location.state}} {{home.location.country}}<br>
+        <img src="/images/star.svg" alt="star.svg" width="20" height="20"> {{home.reviewValue}}
+    </div>
+</template>
+
+<script>
+import homes from '~/data/homes';
+export default {
+    data() {
+        return {
+            home: {}
+        }   
+    },
+    created() {
+        const home = homes.find((home) => home.objectID === this.$route.params._id);
+        this.home = home;
+    },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+     <h2>Something new</h2>
+      <div v-for="home in homes" :key="home.objectID" style="float:left;margin:10px">
+        <nuxt-link :to="`/home/${home.objectID}`"><home-card :home="home"/> </nuxt-link>
+      </div>
+  </div>
+</template>
+
+<script>
+import homes from '~/data/homes';
+
+export default {
+
+  data() {
+    return {
+      homes:homes.slice(0,3)
+    }
+  }
+}
+</script>
